b9ff2e4b151db2843eca6356dfee8b17dadb64f3
punkestu <pangestubima89@gmail.com>
2021-02-09 22:04:11 +0700

    menambahkan fungsi get

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 int main(int argc, char* argv[]){
     qfile file;
     file.init("src/database.bdb");
-    bque::summary(&file);
+    //bque::summary(&file);
+    std::cout<<bque::get(&file, 1, "nama")<<std::endl;
     file.save("src/database.bdb");
     return 0;
 }
